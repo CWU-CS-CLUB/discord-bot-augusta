f6adf6afa4b5d3ab915e55182fc78ed500b5081f
Alice Williams <alice.williams.tech@gmail.com>
2023-04-04 17:46:13 -0700

    working dev version, added joke and help cmds

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 """bot.py: Runs Discord API bot 'Augusta'"""
 
 import discord
+import requests
 
 __author__: 'Alice Williams'
 __version__: '0.0.0'
@@ -10,7 +11,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
-
+api = 'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit'
 
 @client.event
 async def on_ready():
@@ -30,6 +31,12 @@ async def on_message(message):
         await message.channel.send(message.content)
     elif msg.startswith('$help'):
         await message.channel.send('Current commands:\n$help - shows this message\n$hello - says hello back\n$echo <msg> - echos a msg string')
+    elif msg == '$joke':
+        joke = requests.get(api)
+        if "joke" in joke:
+            await message.channel.send(joke["joke"])
+        else:
+            await message.channel.send('joke error occurred')
 
 
 token_file = open('token.txt', 'r')
