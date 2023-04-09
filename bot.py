e4b770b8b7e49b98a89e73a7033e3c5e693b0bf6
ObamaBinModdin <52817466+ObamaBinModdin@users.noreply.github.com>
2023-04-09 01:47:06 -0700

    Three new commands and database
    $users prints a list of all server members.
    $userNum prints number of server members.
    $points prints author's current points.

--- bot.py
+++ bot.py
@@ -4,11 +4,21 @@
 from time import sleep
 import discord
 import requests
+import random
+import constants
+from boto3.dynamodb.conditions import Key
+import DBManagement
+import boto3 as boto3
+from discord.ext import tasks
+
+aws_access_key_id = constants.aws_access_key_id
+aws_secret_access_key = constants.aws_secret_access_key
+region_name = 'us-west-2'
 
 __author__: 'Alice Williams'
 __version__: '0.0.0'
 
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
@@ -18,10 +28,28 @@ help_text = 'Current commands:\n$help - shows this message\n$hello - says hello 
             'string\n$joke - prints a programmer joke '
 
 
+# Runs function every minute. Adds new users and gives out points.
+@tasks.loop(minutes=1)
+async def refreshMembers():
+    await client.wait_until_ready()
+
+    guilds = client.guilds
+
+    for guild in guilds:
+        members = guild.members
+        for member in members:
+            DBManagement.add_user(user_id=member.id, guild_id=member.guild.id, aws_access_key_id=aws_access_key_id,
+                                  aws_secret_access_key=aws_secret_access_key, region_name=region_name)
+            incrementPoints(user_id=member.id, guild_id=member.guild.id)
+
+    print('Refreshed users for all servers.')
+
+
 # Logs in bot.
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+    refreshMembers.start()
 
     
 # Bot does something based on command.
@@ -41,6 +69,12 @@ async def on_message(message):
         await say_help(message)
     elif msg == '$joke':
         await say_joke(message)
+    elif msg == '$users':
+        await list_users(message)
+    elif msg == '$userNum':
+        await member_count(message)
+    elif msg == '$points':
+        await get_points(message)
 
 
 # Greets a user with "Hello!".
@@ -76,6 +110,78 @@ async def say_joke(message):
         await message.channel.send(joke["delivery"])
 
     print("Told a joke.")
+
+          
+# Prints all members or to the limit in the server by their usernames.
+async def list_users(message, limit=-1):
+    # List of all members in the server.
+    members = message.guild.members
+
+    # Shuffles the order of members in the list
+    random.shuffle(members)
+
+    # If there is no limit then print all users.
+    if limit <= 0:
+        for member in members:
+            await message.channel.send(f"{member.display_name}#{member.discriminator}")
+    # Print the specified number of members in the server.
+    else:
+        if limit > len(members):
+            limit = len(members)
+
+        for index in range(limit):
+            await message.channel.send(f"{members[index].display_name}#{members[index].discriminator}")
+
+    print("Printed a list of server members.")
+
+
+# Prints number of members in server.
+async def member_count(message):
+    await message.channel.send('Server member count: {}'.format(message.guild.member_count))
+
+    print('Printed member count.')
+
+
+# Displays user's points.
+async def get_points(message):
+    dynamodb = boto3.resource('dynamodb',
+                              aws_access_key_id=aws_access_key_id,
+                              aws_secret_access_key=aws_secret_access_key,
+                              region_name=region_name
+                              )
+
+    table = dynamodb.Table('Users')
+
+    response = table.query(
+        KeyConditionExpression=Key('user_id').eq(message.author.id) & Key('guild_id').eq(message.guild.id))
+
+    await message.channel.send('You have {} points.'.format(response['Items'][0]['info']['user_points']))
+
+    print('Displayed a user\'s points.')
+
+
+# Increments a user's points based on the incrementalValue.
+def incrementPoints(user_id, guild_id, incrementalValue=1):
+    dynamodb = boto3.resource('dynamodb',
+                              aws_access_key_id=aws_access_key_id,
+                              aws_secret_access_key=aws_secret_access_key,
+                              region_name=region_name
+                              )
+
+    table = dynamodb.Table('Users')
+
+    response = table.update_item(
+        Key={
+            'user_id': user_id,
+            'guild_id': guild_id
+        },
+
+        UpdateExpression=f'set info.user_points = info.user_points + :N',
+        ExpressionAttributeValues={
+            ':N': incrementalValue
+        },
+        ReturnValues='UPDATED_NEW'
+    )
 
 
 token_file = open('token.txt', 'r')
